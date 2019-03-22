555b5f768d9c09788ae0e6ebc780e163af85910f
Filip Bernevec <filipbernevec@gmail.com>
2019-03-22 15:25:57 -0400

    cors fix 3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var cors = require('cors');
 
 const posting = require('./routes/inhouse-job-posting.route'); // Imports routes for the products
 const app = express();
@@ -18,12 +18,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //Solve CORS issues
-app.use(function(req, res, next) {
+
+app.options('*', cors())
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
-});
+}); */
+
+
 
 app.use('/posting', posting);
 
