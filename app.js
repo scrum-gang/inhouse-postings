65f0351bbfe8c7e01ad62bf4825e80673629d1aa
FilipBernevec <filipbernevec@gmail.com>
2019-03-22 01:55:59 -0400

    Cors Issue fix 1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const posting = require('./routes/inhouse-job-posting.route'); // Imports routes for the products
 const app = express();
 
@@ -16,9 +17,16 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Solve CORS issues
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/posting', posting);
 
 let port = 1234;
 app.listen(process.env.PORT || port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port numbner ' + port);
 });
