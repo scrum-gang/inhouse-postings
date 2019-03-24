28732593fb47a0750e909121e930ae9a8e8cbb55
FilipBernevec <filipbernevec@gmail.com>
2019-03-23 21:22:31 -0400

    removed auth for fixing

--- controllers/inhouse-job-posting.controller.js
+++ controllers/inhouse-job-posting.controller.js
@@ -9,13 +9,16 @@ exports.test = function (req, res) {
 };
 
 // Create Posting
-exports.posting_create = async function (req, res) {
-    const authorized = await auth_user_recruiter(req).then(response => response.body.type === RECRUITER);
+//exports.posting_create = async function (req, res) {
+exports.posting_create = function (req, res) {
+    
+    //authentication
+    /*const authorized = await auth_user_recruiter(req).then(response => response.body.type === RECRUITER);
 
     if (!authorized) {
         res.json({"message": "User not an authorized recruiter."});
         return;
-    }
+    }*/
     function getDateTime() {
         var date = new Date();
 
