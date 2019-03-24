51abbe5a0400bb0dc3d93b516f665c7378891af4
Rami <rami.djema@mail.mcgill.ca>
2019-03-24 15:01:44 -0400

    added unit tests

--- test/test.js
+++ test/test.js
@@ -7,6 +7,20 @@ chai.use(require('chai-http'));
 
 const app = require('../app.js'); // Our app
 
+const newUser = {
+	"email": "realperson@realemail.com",
+	"password": "abc123",
+	"type": "Applicant"
+};
+
+const godMode = {
+	"email": "god@realemail.com",
+	"password": "cdb234",
+	"type": "Applicant"
+};
+
+var godToken = null;
+
 describe('Testing inhouse job posting server', function() {
   this.timeout(5000); // How long to wait for a response (ms)
 
@@ -22,9 +36,30 @@ describe('Testing inhouse job posting server', function() {
   it('should reach test endpoint', function() {
     return chai.request(app)
       .get('/posting/test')
+      .set("Authorization", "Bearer " + godToken)
       .then(function(res) {
         expect(res).to.have.status(200);
       });
   });
+
+    // GET - Should test getting all postings for a recruiter
+    it('should get all postings for a recruiter', function() {
+      return chai.request(app)
+        .get('/posting/recruiter/Bob')
+        .set("Authorization", "Bearer " + godToken)
+        .then(function(res) {
+          expect(res).to.have.status(200);
+        });
+    });
+
+    // GET - Should get all postings
+    it('should get all postings for a recruiter', function() {
+      return chai.request(app)
+        .get('/posting/recruiter/allpostings')
+        .set("Authorization", "Bearer " + godToken)
+        .then(function(res) {
+          expect(res).to.have.status(200);
+        });
+    });
 
 });
